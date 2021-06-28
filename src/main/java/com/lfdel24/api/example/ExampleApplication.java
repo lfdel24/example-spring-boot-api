22d0ea5ffe9eff954ae093b9dadcab4135aced21
lfdel24 <lfdel24@gmail.com>
2021-06-27 21:15:10 -0500

    Configurar base de datos

--- src/main/java/com/lfdel24/api/example/ExampleApplication.java
+++ src/main/java/com/lfdel24/api/example/ExampleApplication.java
@@ -1,13 +1,26 @@
 package com.lfdel24.api.example;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+        openChromeLinux();
+    }
 
+    private static void openChromeLinux() {
+        try {
+            URI url = new URI("http://localhost:8080/");
+            Runtime rt = Runtime.getRuntime();
+            rt.exec("/usr/bin/google-chrome-stable %U " + url);
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
